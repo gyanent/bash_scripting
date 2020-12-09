664bbf36b31acbdcc298ed51495ecdf33355273f
Gyanen <gyanenatwork@gmail.com>
2020-12-09 04:19:11 +0000

    12/08

---
+++ simple_function.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+helloWorld() {
+	echo "We are inside helloWorld function"
+}
+
+
+echo "We haven't called the function yet"
+
+echo "Here we call it"
+
+helloWorld
+
+echo "Function is now over. Script is now ending"
+
